973284fbf34ac8bf4310430f062f644a6ec54832
Doug Brown <douglas.brown@mattersight.com>
2018-08-06 18:14:29 -0500

    cleanup python

--- _docker/code/server.py
+++ _docker/code/server.py
@@ -43,27 +43,7 @@ def watchForChanges():
 #		uid = search(dashboardTitle)
 
 		update(dashboard)
-#		print("requesting")
-#		request("http://admin:Abc123!!@grafana.monitoring2.kubernetes.local:32123/api/search", "GET", "")
-#		request("http://admin:Abc123!!@grafana.monitoring2.svc.cluster.local:3000/api/datasources/name/Monitoring", "POST", "")
 
-def search(dashboardTitle):
-	r = requests.Session()
-	retries = Retry(total = 5,
-			connect = 5,
-			backoff_factor = 0.2,
-			status_forcelist = [ 500, 502, 503, 504 ])
-	r.mount('http://', HTTPAdapter(max_retries=retries))
-	r.mount('https://', HTTPAdapter(max_retries=retries))
-	res = r.get("http://admin:Abc123!!@grafana.monitoring2.svc.cluster.local:3000/api/api/search", timeout=10)
-	dashboards = res.json()
-#	print ("search result:")
-#	print (dashboards)
-	xyz = pydash.collections.find(dashboards, lambda db: db["title"] == dashboardTitle)
-#	print ("pydash")
-#	print (xyz)
-	print ("uid={}".format(xyz["uid"]))
-	return xyz["uid"]
 
 def update(dashboard):
 	r = requests.Session()
@@ -81,27 +61,6 @@ def update(dashboard):
 	result = res.json()
 	print ("update result:")
 #	print (result)
-
-def request(url, method, payload):
-	print("in request method")
-	r = requests.Session()
-	retries = Retry(total = 5,
-			connect = 5,
-			backoff_factor = 0.2,
-			status_forcelist = [ 500, 502, 503, 504 ])
-	r.mount('http://', HTTPAdapter(max_retries=retries))
-	r.mount('https://', HTTPAdapter(max_retries=retries))
-	if url is None:
-		print("No url provided. Doing nothing.")
-		# If method is not provided use GET as default
-	elif method == "GET" or method is None:
-		print("doing a get %s" % url)
-		res = r.get("%s" % url, timeout=10)
-		print ("%s request sent to %s. Response: %d %s" % (method, url, res.status_code, res.reason))
-		print (res.json())
-	elif method == "POST":
-		res = r.post("%s" % url, json=payload, timeout=10)
-		print ("%s request sent to %s. Response: %d %s" % (method, url, res.status_code, res.reason))
 
 #app = web.Application()
 #app.router.add_get('/', handle)
